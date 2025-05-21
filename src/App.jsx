ef8945e026f0ea2d1b9b9630ed64f35a52ef1dfa
JJUUNN0 <sophiater7@gmail.com>
2025-05-20 22:40:29 -0300

    Reinício do projeto sem node_modules

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <span className="text-red-600">Hell</span>
+      <span className="text-blue-600">o word</span>
+    </h1>
+  );
+}
